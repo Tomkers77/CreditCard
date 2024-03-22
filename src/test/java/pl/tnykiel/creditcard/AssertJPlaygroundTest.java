b86ac2496be7e4b6e89b7d9a99c12a2fd55a551e
s-007-10 <s-007-10@gumiforest.ae.krakow.pl>
2024-03-22 18:03:28 +0100

    22.03.2024

---
+++ src/test/java/pl/tnykiel/creditcard/AssertJPlaygroundTest.java
@@ -0,0 +1,23 @@
+package pl.tnykiel.creditcard;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.util.Collections;
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AssertJPlaygroundTest {
+
+    @Test
+    void playWithAssertJ(){
+        assertThat("Hello World").contains("Hello");
+
+        assertThat(Collections.emptyList()).hasSize(0);
+
+        assertThat(Instant.now()).isBefore("2023");
+
+        assertThat(new HashMap<>()).containsKey("Tomasz");
+    }
+}
